1eecfd042534aac36a7176785b6650b4a40c5779
j-winston <kenjameswinston@gmail.com>
2022-08-02 09:27:54 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+
+# Get blog posts
+response = requests.get(url="https://api.npoint.io/1c1d666d0ff707b8e0d4")
+blog_json = response.json()
+
+#
+
 
 if __name__ == "__main__":
     app.run(debug=True)
